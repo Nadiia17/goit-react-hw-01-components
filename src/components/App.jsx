f5d74e0981e46bf9da935263ad48911407b5fe3c
Nadiia17 <nababiy@gmail.com>
2023-09-29 16:29:09 +0300

    friend-list

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,15 @@
 import data from './Statistics/data.json';
+import friends from './FriendList/friends.json';
 import { Profile } from './Profile/Profile';
 import { Statistics } from './Statistics/Statistics';
+import { FriendList } from './FriendList/FriendList';
 
 export const App = () => {
   return (
     <>
       <Profile />
       <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />;
     </>
   );
 };
