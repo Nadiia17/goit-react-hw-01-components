557e8d2435c6dc245f07ea3c06f9437a6db0d7df
Nadiia17 <nababiy@gmail.com>
2023-09-29 15:13:51 +0300

    Statistics markup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,12 @@
+import data from './Statistics/data.json';
 import { Profile } from './Profile/Profile';
+import { Statistics } from './Statistics/Statistics';
 
 export const App = () => {
-  return <Profile />;
+  return (
+    <>
+      <Profile />
+      <Statistics title="Upload stats" stats={data} />
+    </>
+  );
 };
